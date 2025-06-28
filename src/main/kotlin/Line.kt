ffbdaccac7783a41ea6dd46eb8757fa7226f561e
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-06-27 18:44:32 -0600

    Update Line.kt

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,4 +1,29 @@
-class Line {
-    // TODO: Implement Line class
+class Line(a: Point, b: Point) {
+    var a: Point = a
+        private set
+
+    var b: Point = b
+        private set
+
+    init {
+        require(a != b) { "A line cannot be formed by identical points (a and b must be distinct)." }
+    }
+
+    fun getSlope(): Double {
+        if (a.x == b.x) return Double.POSITIVE_INFINITY
+        return (b.y - a.y) / (b.x - a.x)
+    }
+
+    fun getLength(): Double {
+        val deltaX = b.x - a.x
+        val deltaY = b.y - a.y
+        return kotlin.math.sqrt(deltaX * deltaX + deltaY * deltaY)
+    }
+
+    // TODO: make an override when Shape class is implemented
+    fun move(deltaX: Double, deltaY: Double) {
+        this.a.move(deltaX, deltaY)
+        this.b.move(deltaX, deltaY)
+    }
 
 }
