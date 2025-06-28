ff0bd1fead5572592e83fbb48b051c19eeb8ad73
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-06-27 18:25:08 -0600

    Update Point.kt

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,4 +1,19 @@
-class Point {
+class Point(x: Double, y: Double) {
     // TODO: Implement Point class
 
+    var x: Double = x
+        private set
+
+    var y: Double = y
+        private set
+
+    fun clone(): Point {
+        return Point(x, y)
+    }
+
+    // make an override when Shape class is implemented
+    fun move(deltaX: Double, deltaY: Double) {
+        this.x += deltaX
+        this.y += deltaY
+    }
 }
