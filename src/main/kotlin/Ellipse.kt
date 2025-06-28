30e8255c0cd4ca51620a4b1b317bbeab32935121
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-06-27 23:21:24 -0600

    Update Ellipse.kt

--- src/main/kotlin/Ellipse.kt
+++ src/main/kotlin/Ellipse.kt
@@ -1,4 +1,27 @@
-class Ellipse {
-    // TODO: Implement Ellipse class
+import kotlin.math.PI
+
+private const val EPSILON = 1e-9
+
+class Ellipse(center: Point, radiusX: Double, radiusY: Double) : Solid() {
+    var center: Point = center
+        private set
+
+    var radiusX: Double = radiusX
+        private set
+
+    var radiusY: Double = radiusY
+        private set
+
+    init {
+        require(radiusX >= EPSILON && radiusY >= EPSILON) { "An ellipse cannot be formed with a non-positive radius." }
+    }
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        center.move(deltaX, deltaY)
+    }
+
+    override fun getArea(): Double {
+        return PI * radiusX * radiusY
+    }
 
 }
